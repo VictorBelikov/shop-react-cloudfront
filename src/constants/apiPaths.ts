d287a717df3afcd1a34753535d41c820d348b820
Victor Belikov <v1ctor.belikov@gmail.com>
2023-05-15 10:40:40 +0300

    feat: add cart api urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://wnrnclkkug.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://9kr9r6eahf.execute-api.us-east-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://noyo37fgri.execute-api.us-east-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -25,10 +25,10 @@ export const handlers = [
     }
     return res(ctx.status(200), ctx.delay(), ctx.json<AvailableProduct>(product));
   }),
-  rest.get(`${API_PATHS.cart}/profile/cart`, (req, res, ctx) => {
+  rest.get(`${API_PATHS.cart}/api/profile/cart`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.delay(), ctx.json<CartItem[]>(cart));
   }),
-  rest.put(`${API_PATHS.cart}/profile/cart`, (req, res, ctx) => {
+  rest.put(`${API_PATHS.cart}/api/profile/cart`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
   rest.get(`${API_PATHS.order}/order`, (req, res, ctx) => {
